a3379181c64721a86b107b9b90b151c9bb98a815
Igor Tarasov <31794450+rasongame@users.noreply.github.com>
2022-07-30 23:07:56 +0500

    Update vending.lua

--- vending.lua
+++ vending.lua
@@ -2,21 +2,20 @@ local component = require("component")
 local event = require("event")
 
 local pim = component.pim
-local stacksToInsert = 2
 local CHEST_SIDE = "west"
 local FILLED_CHEST_SIDE = "north"
+local itemToReplaceDisplayName = "Tin Can"
 local usingME = true
 -- [[
---     Для работы требуется OpenPeripheral
---     В FILLED_CHEST сторону загружаете питательные баночки
---     В CHEST сторону выкидываются использованные баночки 
+--  Для работы требуется OpenPeripheral
+--  В FILLED_CHEST сторону загружаете питательные баночки
+--  В CHEST сторону выкидываются использованные баночки 
 -- ]]
 local chestSlot = 1
 local function replaceCans(slot, i)
     pim.pushItemIntoSlot(CHEST_SIDE, i)
     pim.pullItemIntoSlot(FILLED_CHEST_SIDE, chestSlot, slot.qty, i)
     if not usingME then chestSlot = chestSlot + 1 end
-
 end
 local function scanPlayer()
     while true do
@@ -25,7 +24,7 @@ local function scanPlayer()
         for i = 1, 40 do
             local slot = pim.getStackInSlot(i)
             if slot then
-                if slot.display_name == "Tin Can" then
+                if slot.display_name == itemToReplaceDisplayName then
                     replaceCans(slot, i)
                 end
             end
