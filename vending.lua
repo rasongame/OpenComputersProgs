f5f1d751e75d681d3e1910d353d435824cbdf9d6
Igor Tarasov <31794450+rasongame@users.noreply.github.com>
2022-07-31 08:55:30 +0500

    Update vending.lua

--- vending.lua
+++ vending.lua
@@ -2,33 +2,48 @@ local component = require("component")
 local event = require("event")
 
 local pim = component.pim
-local CHEST_SIDE = "west"
-local FILLED_CHEST_SIDE = "north"
+local CHEST_SIDE = "south"
+local FILLED_CHEST_SIDE = "west"
 local itemToReplaceDisplayName = "Tin Can"
-local usingME = true
+local usingME = false
 -- [[
 --  Для работы требуется OpenPeripheral
 --  В FILLED_CHEST сторону загружаете питательные баночки
 --  В CHEST сторону выкидываются использованные баночки 
--- ]]
+-- ]
 local chestSlot = 1
-local function replaceCans(slot, i)
-    pim.pushItemIntoSlot(CHEST_SIDE, i)
+local chestSlotLimit = 27
+local function replaceCans(slot, i) -- return boolean success
+    local pushRes = pim.pushItemIntoSlot(CHEST_SIDE, i)
+    if pushRes == 0 then return false end -- Если ничё не получилось спихнуть в сундук - сундук полный, а сами не можем положить туда же
     pim.pullItemIntoSlot(FILLED_CHEST_SIDE, chestSlot, slot.qty, i)
-    if not usingME then chestSlot = chestSlot + 1 end
+    if not usingME then 
+        if chestSlot < chestSlotLimit then
+            chestSlot = chestSlot + 1 
+        else
+            print("chestSlot = " .. chestSlot .. " :: Возврат на первую позицию")
+            chestSlot = 1
+        end
+    end
+    return true
 end
 local function scanPlayer()
     while true do
         e, nick, uuid, addr = event.pull("player_on")
+        local replacedCansCount = 0
         print(nick)
         for i = 1, 40 do
             local slot = pim.getStackInSlot(i)
             if slot then
                 if slot.display_name == itemToReplaceDisplayName then
-                    replaceCans(slot, i)
+                    if replaceCans(slot, i) then 
+                        replacedCansCount = replacedCansCount + slot.qty 
+                    end
+
                 end
             end
         end
+        print("Затрачено " .. replacedCansCount .. " банок")
     end
 end
 scanPlayer()
